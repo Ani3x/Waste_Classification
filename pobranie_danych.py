671932154c71afc9ec28b98ab0f1315cc42ed8ca
Ani3x <w.cyfka123@gmail.com>
2026-03-22 14:18:30 +0100

    Delete pobranie_danych.py

--- pobranie_danych.py
+++
@@ -1,9 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="")
-
-project = rf.workspace("nazwa_workspaceu").project("nazwa_projektu")
-
-dataset = project.version(1).download("folder")
-
-print("Dataset został pobrany do folderu:", dataset.location)
